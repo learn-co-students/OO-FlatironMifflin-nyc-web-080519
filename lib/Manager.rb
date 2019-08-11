f79575b41b247d564b1735d8a2837609de0e576d
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-08-11 16:39:50 -0400

    passes all deliverables

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,61 @@
 class Manager
 
-end
+  attr_accessor :name, :department, :age
+
+  @@all = []
+
+  def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    @@all << self
+  end
+
+  # returns an Array of all the managers
+  def self.all
+    @@all
+  end
+
+  # returns an Array of all the department names that every manager oversees
+  def self.all_departments
+    departments = []
+    self.all.each do |manager|
+      departments << manager.department
+    end
+    departments
+  end
+
+  # returns a Float that is the average age of all the managers
+  def self.average_age 
+    total_age = 0
+    self.all.each do |manager|
+      total_age += manager.age 
+    end
+    total_age / self.all.length.to_f
+  end
+
+  # returns an Array of all the employees that the manager oversees
+  def employees
+    Employee.all.select {|employee| employee.manager == self }
+  end
+
+  # takes a String argument and a Fixnum argument of an employee's name
+  # and the employee's salary, respectively, and adds that employee to 
+  # the list of employees that the manager oversees
+  def hire_employee(name, salary)
+    Employee.new(name, salary, self)
+  end
+  
+
+end #end of Manager class
+
+
+
+
+
+# Manager#name
+# returns a String that is the manager's name
+# Manager#department
+# returns a String that is the department that the manager oversees
+# Manager#age
+# returns a Fixnum that is the age of the manager
--- run.rb
+++ run.rb
@@ -4,6 +4,17 @@ require 'pry'
 
 
 #Test your code here
+bill = Manager.new("Bill", "Accounts", 69)
+brenda = Manager.new("Brenda", "HR", 45)
+belinda = Manager.new("Belinda", "Accounting", 35)
+larry = Manager.new("Larry", "Admissions", 56)
+lisa = Manager.new("Lisa", "Marketing", 99)
+
+tracie = lisa.hire_employee("Tracie", 70000)
+kim = lisa.hire_employee("Kim", 75000)
+ingrid = lisa.hire_employee("Ingrid", 100000)
+meagan = larry.hire_employee("Meagan", 69000)
+
 
 
 binding.pry
