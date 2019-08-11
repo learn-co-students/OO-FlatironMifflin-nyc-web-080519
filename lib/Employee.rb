fbf7696ec1ddd7ad4397844859373931a777493b
github username <github email address>
2019-08-11 18:47:11 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,32 @@
 class Employee
+    @@all_employees = []
+    attr_reader :name, :salary, :manager_name
 
+    def initialize(name, salary, manager_name)
+        @name = name
+        @department = department
+        @age = age
+    end
+
+    def self.all
+        @@all_employees
+    end
+
+    def paid_over(salary_number)
+        @@all_employees.select do |employee|
+            employee.salary > salary_number
+        end
+    end
+
+    def find_by_department(department_name)
+        @@all_employees.first do |employee|
+            employee.manager_name.department == department_name
+        end
+    end
+
+    def tax_bracket(self)
+        @@all_employees.select do |employee|
+            employee.salary - self.salary <= 1000|| self.salary - employee.salary <= 1000
+        end
+    end
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,39 @@
 class Manager
+@@all_managers =[]
+attr_reader :name, :department, :age
 
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @managing =[]
+    end
+
+    def employees
+        @managing
+    end
+
+    def self.all 
+        @@all_managers
+    end
+
+    def hire_employee(name, salary)
+        @managing <<  Employee.new(name, salary, self)
+    end
+
+    #_______________NEED TO CHECK THIS ONE ____________________
+    def all_departments
+        @@all_managers.each do |manager|
+            manager.department
+        end
+    end
+    #_______________NEED TO CHECK THIS ONE ____________________
+
+    def average_age
+        total_age = 0
+        @@all.each do |manager|
+             total_age += manager.age
+        end
+        (total_age/ (@@all.length -1))
+    end
 end
