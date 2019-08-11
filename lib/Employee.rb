9be1c7767d35eb7bf3784f8cfafe64ea9aff3b4a
DanBehrman <dan.behrman@wework.com>
2019-08-11 01:39:27 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,35 @@
 class Employee
+    attr_reader :name
+    attr_accessor :salary, :manager_name
 
+    @@all = []
+
+    def initialize(name, salary, manager_name)
+        @name = name
+        @salary = salary
+        @manager_name = manager_name
+        @@all << self
+    end
+
+    def tax_bracket
+        Employee.all.select do |employee|
+            self.salary - employee.salary >= -1000 && self.salary - employee.salary <= 1000
+        end
+    end
+
+    def self.paid_over(amount)
+        all.select do |employee|
+            employee.salary > amount
+        end
+    end
+
+    def self.find_by_department(department)
+        Manager.all.select do |manager|
+            manager.department == department
+        end[0].employees[0]
+    end
+
+    def self.all
+        @@all
+    end
 end
--- run.rb
+++ run.rb
@@ -3,7 +3,14 @@ require_relative "lib/Employee"
 require 'pry'
 
 
-#Test your code here
+m1 = Manager.new("Dwight", "Sales", 38)
+m2 = Manager.new("Darrell", "Warehouse", 32)
+m3 = Manager.new("Francis", "HR", 55)
+
+m1.hire_employee("Jim", 67001)
+m2.hire_employee("Pam", 68000)
+m1.hire_employee("Creed", 90000)
+
 
 
 binding.pry
